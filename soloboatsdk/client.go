58d42e105e13abad4f467f4c7137d0ac11b48768
j <ox19c@foxmail.com>
2019-08-04 11:18:43 +0800

    refine client

--- soloboatsdk/client.go
+++ soloboatsdk/client.go
@@ -1,10 +1,23 @@
 package soloboatsdk
 
+import (
+	"soloos/common/snettypes"
+	"soloos/common/soloosbase"
+)
+
 type Client struct {
-	soloboatServeAddr string
+	soloOSEnv       *soloosbase.SoloOSEnv
+	soloboatWebPeer snettypes.Peer
 }
 
-func (p *Client) Init(soloboatServeAddr string) error {
-	p.soloboatServeAddr = soloboatServeAddr
+func (p *Client) Init(soloOSEnv *soloosbase.SoloOSEnv,
+	soloboatWebPeerID snettypes.PeerID) error {
+	var err error
+
+	p.soloOSEnv = soloOSEnv
+	p.soloboatWebPeer, err = p.soloOSEnv.SNetDriver.GetPeer(soloboatWebPeerID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
